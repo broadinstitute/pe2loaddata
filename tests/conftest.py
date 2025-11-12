b7c200288ab34eed49250a242c71dfa1c690493a
bethac07 <bcimini@broadinstitute.org>
2025-11-12 14:56:37 -0500

    Fix tests from #36

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ import pytest
             'tests/data/Version1/images/Index.idx.xml',
             'tests/data/Version1/config_v1.yml', 
             'tests/data/Version1/load_data_version1.csv',
+            'tests/data/Version1/created_version1.csv',
             {
                 "488 long": "OrigRNA",
                 "Alexa 488": "OrigER",
@@ -21,6 +22,7 @@ import pytest
             'tests/data/Version5/images/Index.idx.xml',
             'tests/data/Version5/config_v5.yml', 
             'tests/data/Version5/load_data_version5.csv',
+            'tests/data/Version5/created_version5.csv',
              {
                 "Brightfield high": "OrigBrightfield",
                 "Alexa 568": "OrigAGP",
@@ -35,6 +37,7 @@ import pytest
             'tests/data/Version7/images/Index.xml',
             'tests/data/Version7/config_v7.yml', 
             'tests/data/Version7/load_data_version7.csv',
+            'tests/data/Version7/created_version7.csv',
                 {
                     "HOECHST 33342": "OrigDNA",
                     "Alexa 568": "OrigAGP",
@@ -48,8 +51,8 @@ import pytest
     ],  
     ids=["v1", "v5", "v7"],)
 def test_files(request):
-    index_directory, index_file, config_file, output_file, expected_channels = request.param
+    index_directory, index_file, config_file, output_file, created_file, expected_channels = request.param
     test_dict = {'index_directory':index_directory,'index_file':index_file,
-                 'config_file':config_file,'output_file':output_file,
+                 'config_file':config_file,'output_file':output_file, 'created_file':created_file,
                  'expected_channels':expected_channels}
     return test_dict
